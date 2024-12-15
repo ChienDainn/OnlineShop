a458d468efc2f40429e0426a72f71181ee8818f1
Chien Minh <chien.minh001@gmail.com>
2024-12-15 23:08:06 +0700

    Update cart section

--- OnlineShop/Controllers/CartController.cs
+++ OnlineShop/Controllers/CartController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OnlineShop.Models.Db;
+using OnlineShop.Models.ViewModels;
 
 namespace OnlineShop.Controllers
 {
     public class CartController : Controller
     {
-        private readonly OnlineShopContext _context;
+        private OnlineShopContext _context;
         public CartController(OnlineShopContext context)
         {
             _context= context;
@@ -13,6 +15,68 @@ namespace OnlineShop.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCart([FromBody] CartViewModel request)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == request.ProductId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Retrieve the list of products in the cart using the dedicated function
+            var cartItems = GetCartItems();
+
+            var foundProductInCart = cartItems.FirstOrDefault(x => x.ProductId == request.ProductId);
+            // If the product is found, it means it is in the cart, and the user intends to change the quantity
+            if (foundProductInCart == null)
+            {
+                var newCartItem = new CartViewModel() { };
+                newCartItem.ProductId = request.ProductId;
+                newCartItem.Count = request.Count;
+
+                cartItems.Add(newCartItem);
+            }
+            else
+            {
+                // If greater than zero, it means the user wants to update the quantity; otherwise, it will be removed
+                if (request.Count > 0)
+                {
+                    foundProductInCart.Count = request.Count;
+                }
+                else
+                {
+                    cartItems.Remove(foundProductInCart);
+                }
+            }
+
+            var json = JsonConvert.SerializeObject(cartItems);
+
+            CookieOptions option = new CookieOptions();
+            option.Expires = DateTime.Now.AddDays(7);
+            Response.Cookies.Append("Cart", json, option);
+
+            var result = cartItems.Sum(x => x.Count);
+
+            return Ok (result);
+
+        }
+        public List<CartViewModel> GetCartItems()
+        {
+            List<CartViewModel> cartList = new List<CartViewModel>();
+
+            var prevCartItemsString = Request.Cookies["Cart"];
+
+            // If it's not null, it means the cart is not empty, so we need to convert it to a list of CartViewModel objects.
+            // Otherwise, we return an empty cart list.
+            if (!string.IsNullOrEmpty(prevCartItemsString))
+            {
+                cartList = JsonConvert.DeserializeObject<List<CartViewModel>>(prevCartItemsString);
+            }
+
+            return cartList;
         }
     }
 }
